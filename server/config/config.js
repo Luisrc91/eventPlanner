503d297dda74cdeadaf59e88a211e572b0b5cfb4
Luis <luisrc91@live.com>
2024-07-23 16:04:50 -0400

    migrated to sql

--- server/config/config.js
+++ server/config/config.js
@@ -1,47 +1,25 @@
-// {
-//   "development": {
-//     "username": "root",
-//     "password": null,
-//     "database": "database_development",
-//     "host": "127.0.0.1",
-//     "dialect": "mysql"
-//   },
-//   "test": {
-//     "username": "root",
-//     "password": null,
-//     "database": "database_test",
-//     "host": "127.0.0.1",
-//     "dialect": "mysql"
-//   },
-//   "production": {
-//     "username": "root",
-//     "password": null,
-//     "database": "database_production",
-//     "host": "127.0.0.1",
-//     "dialect": "mysql"
-//   }
-// }
 require('dotenv').config()
-module.exports ={
+
+module.exports = {
   "development": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
-    "host": "127.0.0.1",
+    "host": "127.0.0.1",    
     "dialect": "postgres"
   },
   "test": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": "eventplanner",
+    "username": "root",
+    "password": null,
+    "database": "database_test",
     "host": "127.0.0.1",
-    "dialect": "postgres"
+    "dialect": "mysql"
   },
   "production": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": "eventplanner",
+    "username": "root",
+    "password": null,
+    "database": "database_production",
     "host": "127.0.0.1",
-    "dialect": "postgres"
+    "dialect": "mysql"
   }
 }
--- server/models/event.js
+++ server/models/event.js
@@ -4,30 +4,68 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Event extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    static associate({ User }) {
+      Event.belongsTo(User, { as: 'user', foreignKey: 'userId' });
     }
   }
-  Event.init({
-    eventName: DataTypes.STRING,
-    partyType: DataTypes.STRING,
-    inviteNames: DataTypes.TEXT,
-    numberOfInvites: DataTypes.INTEGER,
-    foodType: DataTypes.STRING,
-    place: DataTypes.STRING,
-    band: DataTypes.STRING,
-    eventTime: DataTypes.TIME,
-    eventDate: DataTypes.DATE,
-    address: DataTypes.STRING,
-    eventSize: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Event',
-  });
+  Event.init(
+    {
+      eventName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      partyType: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      inviteNames: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      numberOfInvites: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      foodType: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      place: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      band: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      eventTime: {
+        type: DataTypes.TIME,
+        allowNull: false,
+      },
+      eventDate: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      address: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      eventSize: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Event',
+    }
+  );
   return Event;
 };
