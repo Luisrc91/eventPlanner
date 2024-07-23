d8d9ec1bf99a8b84dd0397a05c14b7e26c6454b0
Luis <luisrc91@live.com>
2024-07-23 15:44:17 -0400

    updated user models

--- server/models/user.js
+++ server/models/user.js
@@ -8,8 +8,15 @@ module.exports = (sequelize, DataTypes) => {
   }
   User.init(
     {
-      username: DataTypes.STRING,
-      password: DataTypes.STRING,
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
     {
       sequelize,
