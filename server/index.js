b8329a143a8fedbbe30ad07b1045c448db90596c
Luis <luisrc91@live.com>
2024-07-25 14:37:27 -0400

    created server

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { sequelize } = require('./models');
+
+const app = express();
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 3000;
+
+const userRoutes = require('./models/user');
+const eventRoutes = require('./models/event');
+
+app.use('/users', userRoutes);
+app.use('/events', eventRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, async () => {
+  console.log(`Server is running on port ${PORT}`);
+  await sequelize.authenticate();
+  console.log('Database connected!');
+});
