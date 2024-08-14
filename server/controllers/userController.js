85c156d6d4d07aa7c508546cd24811e58c314ba3
Luis <luisrc91@live.com>
2024-08-14 14:41:43 -0400

    added login user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,6 @@
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const { User } = require("../models");
-
 
 // creates user
 const register = async (req, res) => {
@@ -22,6 +21,28 @@ const register = async (req, res) => {
   }
 };
 
+// login user
+const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ where: { username } });
+    if (!user) {
+      return res.status(401).json({ error: "User not found" });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.json({ message: "Login successful", token });
+  } catch (error) {
+    res.status(500).json({ error: "login failed" });
+  }
+};
+
 module.exports = {
   register,
+  login,
 };
--- server/routes/user.js
+++ server/routes/user.js
@@ -5,4 +5,6 @@ const userController = require('../controllers/userController');
 // Define routes for user sign up
 router.post('/register', userController.register);
 
+router.get('/login', userController.login);
+
 module.exports = router;
